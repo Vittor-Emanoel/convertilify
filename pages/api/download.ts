2bf4741dc83045c0643e196821b40baa06f37479
vittoremanoel1234@gmail.com <vittoremanoel1234@gmail.com>
2024-04-29 23:19:09 -0300

    fix - poderia funcionar

--- pages/api/download.ts
+++ pages/api/download.ts
@@ -1,4 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { pipeline } from "stream";
+import { promisify } from "util";
 import ytdl, { getBasicInfo } from "ytdl-core";
 
 export const config = {
@@ -7,6 +9,7 @@ export const config = {
     responseLimit: false,
   },
 };
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -17,19 +20,39 @@ export default async function handler(
     if (!url) {
       throw new Error("URL não fornecida.");
     }
+
     const info = await getBasicInfo(url);
+
+    if (!info) {
+      throw new Error("Informações do vídeo não encontradas.");
+    }
 
     res.setHeader(
       "Content-Disposition",
-      `attachment; filename=${info.videoDetails.title}.mp3`
+      `attachment; filename="${info.videoDetails.title}.mp3"`
     );
-    ytdl(url, {
+
+    const videoReadableStream = ytdl(url, {
       filter: "audioonly",
       quality: "lowestaudio",
-    }).pipe(res);
+    });
+
+    let contentLength = 0;
+
+    // Calcular o tamanho total do arquivo
+    videoReadableStream.on("response", (res) => {
+      contentLength = parseInt(res.headers["content-length"] || "0");
+    });
+
+    // Transmitir o vídeo para a resposta
+    await promisify(pipeline)(videoReadableStream, res);
+
+    // Definir cabeçalho para o tamanho total do arquivo
+    res.setHeader("Total-Bytes", contentLength.toString());
 
     res.status(200);
   } catch (error) {
+    console.error("Erro:", error);
     res.status(500).json({ error: "Ocorreu um erro." });
   }
 }
